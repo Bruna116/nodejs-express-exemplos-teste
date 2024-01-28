fd3ff9bf52bd98a3d0b376f700af115731e92dad
Bruna116 <bruninha.charmosinha@hotmail.com>
2024-01-27 21:05:35 -0300

    Criado middleware de autenticação

--- src/modules/post/postController.js
+++ src/modules/post/postController.js
@@ -1,12 +1,13 @@
 import { Router } from 'express'
+import { verifyAccessToken } from '../../utils/auth'
 
 const router = Router()
 
-router.post('/', (req, res) => {
+router.post('/', verifyAccessToken, (req, res) => {
   res.send('CRIATE POST /')
 })
 
-router.get('/:id?', (req, res) => {
+router.get('/:id?', verifyAccessToken, (req, res) => {
   // DUAS OPIÇÕE: LISTAR TODOS OU APENAS UM
   res.send('GET POST /')
 })
